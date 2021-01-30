4926afa1b3087b22930c070c96afc1361ca90572
dimonlev <dimon_lev@mail.ru>
2021-01-30 22:49:51 +0300

    feat: dream-team done

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  dreamTeam = [];
+  if (arr === []) {
+    return []
+  }
+  else if (!!arr) {
+    for (let i = 0; i < arr.length; i++) {
+      const el = arr[i];
+      if (typeof el === typeof 'Dima')
+        dreamTeam.push(el.trim()[0].toUpperCase())
+    }
+    dreamTeam = dreamTeam.sort().join("");
+    return dreamTeam;
+  }
+  else return false
+
+
 };
